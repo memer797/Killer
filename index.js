45fdb048a14eff0b1a3ca8177407680bd047bf15
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-30 21:36:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var express = require("express");
-var app = express();
-app.listen("3000");
+async function RunMainCode(){
+function delay(milliseconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, milliseconds);
+  });
+};
+  const express = require("express"),
+    bodyParser = require('body-parser'),
+    path = require("path"),
+    cookieParser = require("cookie-parser"),
+    app = express(),
+    server = app.listen(80),
+    io = require('socket.io')(server);
+
+
+  
 app.get("/", async(req, res) => {
 res.send("Hello Rahul");
 });
+
+
+};
+RunMainCode();
