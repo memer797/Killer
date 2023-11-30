194554edbc6936c89e15c9bdb62ab7039dc5788c
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-30 21:42:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//express body-parser path cookie-parser socket.io ejs
 async function RunMainCode(){
 function delay(milliseconds) {
   return new Promise(resolve => {
@@ -9,8 +10,27 @@ function delay(milliseconds) {
     path = require("path"),
     cookieParser = require("cookie-parser"),
     app = express(),
-    server = app.listen(80),
+    server = app.listen(3000),
     io = require('socket.io')(server);
+
+  //using modules
+  app
+    .use(cookieParser("putaMexican_WtfItsASong", {
+      maxAge: 900000 * 4 * 24 * 5,
+      secure: true,
+    }))
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(bodyParser.urlencoded({ extended: true }))
+    .engine("html", require("ejs").renderFile)
+    .set("view engine", "ejs")
+    .use(express.static(path.join(__dirname, "/public")))
+    .set("views", path.join(__dirname, "/views"))
+  io.use((socket, next) => {
+    const sockcookies = socket.request.headers.cookie;
+    socket.cookies = sockcookies;
+    next();
+  });
 
 
   
