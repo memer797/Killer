c19f08fd888eb120b9482f614e7d05ba3d252ae5
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-30 15:03:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
-app.listen(3000);
+app.listen("3000", function() => {
+  console.log("Server is running on port: 3000");
+});
 app.get("/", async(req, res) => {
 res.send("Hello Rahul");
 });
