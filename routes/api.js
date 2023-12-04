dc46acf5b26cf49c8f4787762258d41cc82be3a8
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-12-04 15:12:29 +0530

    [render skip]

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,12 @@
 var express = require("express");
 var router = express.Router();
+
+router.get('/', async(req, res) => {
+res.send({ success: true, msg; 'api route' });
+});
+
+router.get('/data', async(req, res) => {
+res.json({ success: true, msg: 'data route' });
+}):
+
+module.exports = router;
