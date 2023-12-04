b12d746acadf9330fb556b448d4d194ccd2a8973
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-12-04 15:16:46 +0530

    [render skip]

--- routes/api.js
+++ routes/api.js
@@ -8,5 +8,11 @@ res.send({ success: true, msg; 'api route' });
 router.get('/data', async(req, res) => {
 res.json({ success: true, msg: 'data route' });
 }):
-
+router.get('/data/movie/:id', async(req, res) => {
+var id = req.params.id;
+  if(!id || id.trim() === ''){
+  return res.json({ success: false, msg: 'id is not provided!'});
+};
+  
+});
 module.exports = router;
