d060612516fa99d1897a2300b76efba35668cdb1
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-12-05 14:22:52 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,11 @@
 var express = require('express');
-var router = express.Router();;
+var router = express.Router();
+ router.use("*", async(req, res, next) => {
+if(!req.cookies.admin_key || req.cookies.admin_key !== process.env.admin_login_cookie){
+ return res.render("admin/login");
+ }
+next();
+ });
 
 router.get("/", async(req, res) => {
 res.send("hi");
