0a7d21b879f8f8c9049102fc22979cd0c2669623
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-12-05 22:51:12 +0530

    [render skil]

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,22 @@ if(!req.cookies.admin_key || req.cookies.admin_key !== process.env.admin_login_c
  }
 next();
  });
+//post routes for data handling
+router.post("/data/save/movie", async(req, res) => {
+if(!req.cookies.admin_key || req.cookies.admin_key !== process.env.admin_login_cookie){
+ return res.json({success: false, msg: " err_authontication"});
+}
+ res.json({success: true, msg: "movie saves successfully"});
+});
+router.post("/data/get/movie", async(req, res) => {
+if(!req.cookies.admin_key || req.cookies.admin_key !== process.env.admin_login_cookie){
+ return res.json({success: false, msg: " err_authontication"});
+}
+ var movAray = db
+ res.json({success: true, data: movAray});
+});
 
+//get methods to serve files
 router.get("/", async(req, res) => {
 res.render("admin/panel");
 });
