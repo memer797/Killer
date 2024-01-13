9f410bce6b3e79aedb0b87d372b4a82f0b760c3c
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-01-13 13:48:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ console.log(data);
 
       var fuse = new searchAlgo__(data, options_s);
       var result = fuse.search(query);
+     res.render("search list", {list: result.map(item => ({name: item.item.name, id: item.item.id, category: item.item.category})});
       res.json(result.map(item => ({name: item.item.name, id: item.item.id})));
 });
 
