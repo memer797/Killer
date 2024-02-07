a12c2f3abdefc66babeb14427b13c86b916579f1
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-02-07 19:04:34 +0530

    [render skip]

--- routes/chat.js
+++ routes/chat.js
@@ -1,1 +1,15 @@
+var express = require("express");
+var router = express.Router();
 
+
+router.get("/", async(req, res) => {
+  res.send("Chat Page");
+});
+
+//message store
+router.get("/message/store", async(req, res) => {
+res.json([{}]);
+});
+
+
+module.exports = router;
