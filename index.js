178807f0a2819e994201d89a454a46f6c00df7c0
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-02-09 23:54:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -236,10 +236,12 @@ app.get("/sitemap/dynamic/all-movies.xml", (req, res) => {
 res.header('Content-Type', 'application/xml');
   res.send(`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 <url>
-<loc>https://all-yt.onrender.com/ff/giveaway</loc>
+${[1, 2, 3].forEach(data => {
+`<loc>${data}</loc>
 <lastmod>${FakeDateToShowInGoogle}</lastmod>
 <changefreq>weekly</changefreq>
-<priority>0.80</priority>
+<priority>0.80</priority>`
+                    });}
 </url>
 </urlset>`);
     
