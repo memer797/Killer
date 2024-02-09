3f9d152982ff029c1ee6976732346f2f81520578
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-02-09 21:57:12 +0530

    [render skip]

--- socket/public.chat.js
+++ socket/public.chat.js
@@ -1,3 +1,11 @@
 module.exports = async function(io) {
+  
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
 }
