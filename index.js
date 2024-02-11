d8cdd315191fc6fcb22ed98522750eb3a36f3465
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-02-11 14:58:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ let disabled = true;
     app.use("*", async(req, res, next) => {
 
 if(!req.cookies.admin_key || req.cookies.admin_key !== process.env.admin_login_cookie){
-      if(req.path == '/login/admin'){ next(); }
+      if(req.originalUrl == '/login/admin'){ next(); }
   return res.send("We are updating the site");
  }
 next();
