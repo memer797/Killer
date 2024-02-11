14138f82b0ddf7bcc6478543a1a4cd02a47e3efd
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-02-11 19:03:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,9 +64,11 @@ var val = req.query.value;
   */
 app.get("/", async (req, res) => {
     var data = await db.getArray("info.movie");
-var result = Object.values(data.reduce((acc, { name, category, img, id }) => {
+var result = Object.values(data.reduce((acc, { name, categoryArray, img, id }) => {
+  categoryArray.forEach(category => {
   acc[category] = acc[category] || { category, items: [] };
   acc[category].items.push({ name, img, id });
+  }
   return acc;
 }, {}));
 
