f67f24c9f99280211845d82ed2e42d06289c1172
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-02-14 16:57:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function delay(milliseconds) {
   console.log(!(req.originalUrl.startsWith("/admin")));
 var ip = req.ip;
 
-    if(!(req.originalUrl.startsWith("/admin")) || !(req.originalUrl.startsWith("/ping")) || req.method.toLowerCase() === "get"){
+    if(/*!(req.originalUrl.startsWith("/admin")) || !(req.originalUrl.startsWith("/ping")) || */req.method.toLowerCase() === "get"){
     global.totalMagaViews++;
     if(!(viw.has(ip))){
    global.uniqueMagaViews++;   
