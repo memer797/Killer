f9457ad042e5a4c0759ea7e579b000141665a6ae
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-02-16 15:23:36 +0530

    [render skip]

---
+++ routes/pages.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router(
+
+router.get('/see-all', async(req, res) => {
+var pageNum = req.query.pageN;
+if(!pageNum || pageNum.trim() === '' || isNaN(pageNum)){
+return res.json({success: false, error: 'invalid page number provided'});
+}
+// here to do
+});
+
+router.get('/category', async(req, res) => {
+var pageNum = req.query.pageN;
+if(!pageNum || pageNum.trim() === '' || isNaN(pageNum)){
+return res.json({success: false, error: 'invalid page number provided'});
+}
+// here to do
+});
+
+router.get('/tags', async(req, res) => {
+var pageNum = req.query.pageN;
+if(!pageNum || pageNum.trim() === '' || isNaN(pageNum)){
+return res.json({success: false, error: 'invalid page number provided'});
+}
+// here to do
+});
+
+
+
+
+
+module.exports = router;
