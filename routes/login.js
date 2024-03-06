6386500e599b685b0b745928c36ad5a2f000a5e7
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-03-06 15:21:30 +0530

    [render skip]

--- routes/login.js
+++ routes/login.js
@@ -1,1 +1,8 @@
+var express = require('express');
+var router = express.Router();
 
+router.get('/', async(req, res) => {
+res.send('<h1>login route needs a login page <br> so build a login page dear Rahul also build a signup page 🙂!</h1>');
+});
+
+module.export = router;
