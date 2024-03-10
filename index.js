1915869ca992d6cc92f455983a7afaea66ca9489
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-03-10 10:28:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -140,7 +140,41 @@ var data = await db.getArray("info.movie");
 
   global.srarchTermRecord.push(`${query} [•|{\\:/}|•] ${result[0] ? result[0].item.name : "404"}`); 
  });
-  
+
+  app.get("/movie/jawan", async(req, res) => {
+
+var id = "lYdLsL4I17RcgcvZj11C";
+  var movie_info = await db.getArray("info.movie");
+  var movie_info = movie_info.filter(i => i.id === id);
+  if(!movie_info || movie_info.length < 1){
+    return res.send(`4** error, N 0 Movie F()und With ${id} id`);
+  }
+  console.log(movie_info);
+  var name = movie_info[0].name;
+  var description = movie_info[0].description;
+  var img = movie_info[0].img;
+  var category = movie_info[0].category;
+  var tags = movie_info[0].tags;
+  var duration = movie_info[0].duration;
+  var language = movie_info[0].language;
+  var release_date = movie_info[0].release_date;
+  var cast = movie_info[0].cast;
+  var links = movie_info[0].links;
+  res.render("show_movie", {
+    name: name,
+    banner: {
+      jpg: img
+    },
+    release_date: release_date,
+    duration: duration,
+    category: category,
+    actors: cast,
+    language: language,
+    description: description,
+    downloads: links,
+    tags
+  });
+ });
 
 app.get("/movie/:id", async(req, res) => {
   if(!req.params.id || req.params.id.trim() === ""){
