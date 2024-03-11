2a3f82325b7ca255b02a520e870c3a8670f64251
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-03-11 17:11:12 +0530

    [skip render]

--- routes/adminDM.js
+++ routes/adminDM.js
@@ -1,5 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
+router.post("/query", async(req, res) => {
+var senderName = req.body.name;
+var query = req.body.q;
+  if(!senderName || !query){
+    return res.json({
+      success: false,
+      msg: `${senderName ?? ""} ${senderName ? query ? "" : "and"  : ""} is required!!`
+    });
+  }
+})
 
 module.exports = router;
