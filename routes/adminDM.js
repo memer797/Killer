bab6f73c0f492cfa47b2780f0842a5825198eee8
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-03-12 22:26:38 +0530

    [skip render]

--- routes/adminDM.js
+++ routes/adminDM.js
@@ -20,6 +20,26 @@ var query = req.body.q;
     sender: senderName,
     query: query
   });
-})
+});
+
+router.delete("/query", async(req, res) => {
+var senderName = req.query.name;
+var query = req.query.q;
+  if(!senderName || !query){
+    return res.json({
+      success: false,
+      msg: `${senderName ? "" : "name"}${senderName ? "" : query ? "" : " and" }${query ? "" : " query"} is required!!`
+    });
+  res.json({
+    success: true,
+    msg: "query has been deleted"
+  });
+    try {
+    await db.pull("user.query", {
+    sender: senderName,
+    query: query
+  });
+    }catch{};
+});
 
 module.exports = router;
