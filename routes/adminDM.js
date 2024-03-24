45946cd91b6d66cdb478933c805a38782f6e6ae7
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-03-24 12:46:15 +0530

    [skip render]

--- routes/adminDM.js
+++ routes/adminDM.js
@@ -5,7 +5,6 @@ var wsAdminDm = socketIo.of("/ws/dm/admin");
 router.post("/query", async(req, res) => {
 var senderName = req.body.name;
 var query = req.body.q;
-var ip = req.ip || '0.0.0.0';
   console.log(ip);
   if(!senderName || !query){
     return res.json({
@@ -20,15 +19,13 @@ var ip = req.ip || '0.0.0.0';
   wsAdminDm.emit("new.query", { sender: senderName, query: query });
     await db.push("user.query", {
     sender: senderName,
-    query: query,
-    ip: ip || '0.0.0.0'
+    query: query
   });
 });
 
 router.delete("/query", async(req, res) => {
 var senderName = req.body.name;
 var query = req.body.q;
-var ip = req.body.ip || '0.0.0.0';
   if(!senderName || !query){
     return res.json({
       success: false,
@@ -42,8 +39,7 @@ var ip = req.body.ip || '0.0.0.0';
     try {
     await db.pull("user.query", {
     sender: senderName,
-    query: query,
-    ip: ip
+    query: query
   });
     }catch{};
 });
