b74d15622053474a32437c395d5be09bc5edea5f
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-17 11:19:30 +0530

    [skip render]

--- routes/support-chat.js
+++ routes/support-chat.js
@@ -4,3 +4,11 @@ var router = express.Router();
 router.get("/", async(req, res) => {
 res.send("support chat page will show here");
 });
+
+router.get("/chat/:id", async(req, res) => {
+res.send(`opening chat id: ${req.params.id}`);
+});
+
+router.post("", async(req, res) => {
+ res.send({success: true, chat_id: "xyz"}); 
+});
