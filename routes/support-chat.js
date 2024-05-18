2c0a7ffb32ceb31f02430378161dbca9109b6ffb
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-18 17:34:20 +0530

    [skip render]

--- routes/support-chat.js
+++ routes/support-chat.js
@@ -2,25 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", async(req, res) => {
+  let userCok = req.cookie.guestSupportChat;
 res.send("support chat page will show here");
 });
 
-router.get(/chat/create', async(req, res) => {
-  res.json({});
-});
-
 router.get("/chat/confirm/:id", async(req, res) => {
+  let userCok = req.cookie.guestSupportChat;
   res.json({});
 });
 
 router.get("/chat/:id", async(req, res) => {
+  let userCok = req.cookie.guestSupportChat;
 res.render(`support-chat`);
 });
 
 router.post("/chat", async(req, res) => {
+  let userCok = req.cookie.guestSupportChat;
   let { title, name } = req.body;
   res.json({success: true, id: `somthing random unique id", msg: "Support Chat Created Successfully ✓ named ${title}`});
 });
+
 router.delete("/chat/:id", async(req, res) => {
  res.json({success: true, msg: `${req.params.id} Chat Deleted ✓`}); 
 });
