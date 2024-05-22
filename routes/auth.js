cf48e558b30f80535e76fa5dce9d83603c592c16
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-22 14:17:56 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var router = express.Router();
 
+router.get("*", async(req, res, next)=> {
+  next();
+});
+
+router.get("/login", async(req, res) => {
+  res.render("auth/index");
+});
+
+router.get("/signup", async(req, res) => {
+  res.send("Signup Page hobek");
+});
+
+router.get("*", (req, res) => {
+  res.send("huh!? ato kon link?");
+});
 
 
-module.export = router;
+module.exports = router;
