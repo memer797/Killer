e0b37797ace6baf05d8bca9f545ffa1aef784314
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-30 23:36:24 +0530

    Update webseries-route.js

--- routes/webseries-route.js
+++ routes/webseries-route.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-res.send("Webseries page");
+res.send("display.render(page[home].webseries())");
 });
 
 module.exports = router;
