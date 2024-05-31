5a008d10100454f5a0b09d2a55680b37145072b1
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-31 23:24:06 +0530

    Update webseries-route.js

--- routes/webseries-route.js
+++ routes/webseries-route.js
@@ -1,8 +1,35 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", (req, res) => {
-res.send("display.render(page[home].webseries())");
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+router.get("/", async(req, res) => {
+  var data = await db.getArray("info.movie");
+  var data = data.filter(item => item.contentType === "webseries");
+    var trendDataNow =  await db.getArray("trend.movie");
+  const result = Object.values(data.reduce((acc, { name, category, img }) => {
+  category.forEach(category => {
+    acc[category] = acc[category] || { category, items: [] };
+    acc[category].items.push({ name, img });
+    // Shuffle the items array and keep only the first 5 items
+    acc[category].items = shuffle(acc[category].items).slice(0, 10);
+  });
+  return acc;
+}, {}));
+let tmdtal = [];
+  trendDataNow.forEach(g => {
+var udf = data.filter(j => j.name === g && j.contentType === "webseries");
+    if(udf.length > 0){
+      tmdtal.push({name: udf[0].name, img: udf[0].img }); 
+    }
+  });
+  res.render('index', { data: result, trend: tmdtal });
 });
 
 module.exports = router;
