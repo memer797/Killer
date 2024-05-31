05cc87ff57fe7441d43eea97fcf4c4aa9b47806d
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-31 20:10:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ var adminDM = require("./routes/adminDM");
 var movieRoute = require("./routes/movie-route");
 var supportChat = require("./routes/support-chat");
 var authRoute = require("./routes/auth");
+  
   global.webDisabled = false;
   app.use("/api", apiRoute);
   app.use("/admin", adminRoute);
@@ -89,6 +90,7 @@ var authRoute = require("./routes/auth");
   app.use("/webseries", require("./routes/webseries-route.js"));
   app.use("/support", supportChat);
   app.use("/auth", authRoute);
+  app.use("/download", ());
 app.use("*", async(req, res, next) => {
 if(global.webDisabled){
       if(req.originalUrl == '/login/admin'){ return next(); }else if(!req.cookies.admin_key || req.cookies.admin_key !== process.env.admin_login_cookie){
