22ce4bbef9e34eadd6a58fa1f527c7efc3bc2b8b
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-07-04 00:41:45 +0530

    [skip render]

--- socket/admin.todo.js
+++ socket/admin.todo.js
@@ -3,3 +3,17 @@ var todoSocket = socketIo.of("/ws/admin/todo");
 todoSocket.on("connection", (socket) => {
   
 });
+
+
+//"get.list"
+/**
+* todos structure
+* [
+*   {
+*      title: String,
+*      isDone: Boolean,
+*      isDeleted: Boolean,
+*    },
+*    ...
+* ]
+**/
