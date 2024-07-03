9eea08d941c502ab3d82b5b9188b803fd390df11
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-07-03 15:51:29 +0530

    Update notification.js

--- routes/notification.js
+++ routes/notification.js
@@ -1,1 +1,18 @@
+var express = require('express');
+var router = express.Router();
 
+router.get("/", async(req, res) => {
+  res.send("cannot get './notification.html' | unable to read notification.html");
+});
+
+router.post("/subscribe", async(req, res) => {
+  res.send("no code to evaluate");
+});
+
+router.get("/get/:id", async(req, res) => {
+  //
+});
+
+router.post("/perform-search", async(req, res) => {
+  let query = req.query.term;
+});
