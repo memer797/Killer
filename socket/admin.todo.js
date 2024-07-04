e28b04bc78cceb564aa7258c13404e63daf46019
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-07-04 19:21:36 +0530

    [skip render]

--- socket/admin.todo.js
+++ socket/admin.todo.js
@@ -1,15 +1,15 @@
 var todoSocket = socketIo.of("/ws/admin/todo");
 
 todoSocket.on("connection", (socket) => {
-  socket.on("get.list", (req) => {
-    req([{
-      name: "Test",
-      isDone: false,
-      isDeleted: false,
-    }]);
+  socket.on("get.list", async(req) => {
+    let allTodos = await db.getArray("admin.todos");
+    req(allTodos);
   });
 
-  socket.on("new.todo", (req
+  socket.on("new.todo", async(req) => {
+    if(!req || req.trim() === "" || typeof req !== "string") return;
+  await db.push("admin.todos", req)
+  });
 });
 
 
