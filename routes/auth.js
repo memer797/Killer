a88229966b47c52fd307cb644979395786f8f2ee
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-07-06 18:26:00 +0530

    [skip render]

--- routes/auth.js
+++ routes/auth.js
@@ -31,8 +31,16 @@ router.get("/signup", async(req, res) => {
   res.render("auth/signup");
 });
 router.post("/signup", async(req, res) => {
+  let otpMail = {
+    from: `"Verification" <${senderEmailAddress}>`,
+    to: "sujoyk211@gmail.com",
+    subject: "OTP Verification",
+    text: `Your OTP is: #### 
+    Use This OTP to Verify Your Email Address
+    » Don't Share This OTP to anyone except memer797 admin`
+  };
   
-  otpSender.sendMail(withtext, function(error, info) {
+  otpSender.sendMail(otpMail, function(error, info) {
   if (error) {
     console.log(error);
     res.json({ success: false, msg: "Error while sending Email" });
