135e7aa7627c2c3eef961cb6aec76b742e29e6ce
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-08-06 12:18:03 +0530

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -1,3 +1,44 @@
 let router = require('express').Router();
+let db = {
+	
+};
+router.use('*', async(req, res, next) => {
+  next();
+});
+
+router.get('/', async(req, res) => {
+	res.send('@account "/"');
+});
+
+router.get('/settings', async(req, res) => {
+	res.json({ error: false, data: {
+		name: 'sujoy',
+		username: 'duplicate2991',
+		userId: 1234567890,
+	}});
+});
+
+router.post('/settings', async(req, res) => {
+	let newData = {
+		name: "Sujoy",
+		username: "sujoy2991",
+		lastUpdated: Date.now(), 
+		};
+	db.save('userId', newData);
+	res.json({
+		error: false,
+		data: {
+			name: "Sujoy",
+			username: "sujoy2991",
+			userId: 1234567890,
+			lastUpdated: Date.now(),
+			},
+		alert: true, 
+		message: { 
+			type: 'success', 
+			content: "Profile Saved Successfully" 
+			}
+	 });
+});
 
 module.exports = router;
